eaaeda1af6234d04ac710c28545fad6514004f7e
Arturo <lemosart0987@gmail.com>
2025-09-10 15:52:01 -0500

    Third commit adjusted the variables to make more sense, changed variable counting the number of each flavor from flavor to flavorCount.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const order = [];
+const order = []; //Created an empty array called order to store the user inputs.
 let flavors = prompt(
   "Please input your flavor. Use a comma to separate each order. For ex: vanilla,strawberry,coffee,coffee (this will order 1 vanilla, 1 strawberry, and 2 coffee)"
-);
-let orderList = flavors.split(",");
+); //Prompt that explains how user should input their flavor selections.
+let orderList = flavors.split(","); //Declared variable orderList which is the result of the flavors input turned into strings and separated by commas.
 
 for (let i = 0; i < orderList.length; i++) {
-  order.push(orderList[i].trim());
+  order.push(orderList[i].trim()); //For loop that pushes flavor inputs into the order variable.
 }
-console.log("Your order:", order);
-const count = {};
+console.log("Your order:", order); //console log the order.
+const count = {}; //Create an empty object called count.
 for (let i = 0; i < order.length; i++) {
-  let flavor = order[i];
-  if (count[flavor]) {
-    count[flavor]++;
+  let flavorCount = order[i]; //for loop
+  if (count[flavorCount]) {
+    count[flavorCount]++;
   } else {
-    count[flavor] = 1;
+    count[flavorCount] = 1;
   }
 }
 
