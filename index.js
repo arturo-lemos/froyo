45907767734521035a4ea3fab15e1ba11dcfdc6a
Arturo <lemosart0987@gmail.com>
2025-09-10 15:35:38 -0500

    second commit added array order, variable flavors with  prompt, variable orderList that splits flavors into strings separated by commas. Added for loop to push inputs into orderList. Added object c ount with for loop to count the number of each flavor and console.log the results.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-prompt("Select a flavor");
+const order = [];
+let flavors = prompt(
+  "Please input your flavor. Use a comma to separate each order. For ex: vanilla,strawberry,coffee,coffee (this will order 1 vanilla, 1 strawberry, and 2 coffee)"
+);
+let orderList = flavors.split(",");
+
+for (let i = 0; i < orderList.length; i++) {
+  order.push(orderList[i].trim());
+}
+console.log("Your order:", order);
+const count = {};
+for (let i = 0; i < order.length; i++) {
+  let flavor = order[i];
+  if (count[flavor]) {
+    count[flavor]++;
+  } else {
+    count[flavor] = 1;
+  }
+}
+
+console.log("Your flavors:", count);
